1797958fa50f9c515589dac4c7ec9c816236ac81
Jagadish <Jagadish.shenoy@philips.com>
2017-12-31 13:14:30 +0100

    Add the sample project with Java plugin issue

---
+++ lib/src/main/java/com/example/lib/Lib.java
@@ -0,0 +1,10 @@
+package com.example.lib;
+
+import com.example.lib2.Lib2;
+
+public class Lib {
+
+    public static void main(String[] args) {
+        new Lib2().sayHello();
+    }
+}
---
+++ lib2/src/main/java/com/example/lib2/Lib2.java
@@ -0,0 +1,8 @@
+package com.example.lib2;
+
+public class Lib2 {
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+}
